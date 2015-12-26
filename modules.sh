0ec9849a63aaa990cc9a0d242f7fe66a59aa9381
WildfireDEV <wildfiredev@gmail.com>
2015-12-26 18:01:45 +1100

    Improve modules.sh script

--- modules.sh
+++ modules.sh
@@ -4,6 +4,15 @@
 # To use, simply just run the script before compiling
 #
 
-mkdir ../kernel_output_flar2
-cp arch/arm/boot/zImage ../kernel_output_flar2/zImage
-find . -name "*.ko" -exec cp {} ../kernel_output_flar2 \;
+if [ ! -d /kernel_output_flar2 ]; then
+    cd ../kernel_output_flar2
+    rm -rf *
+    cd ~
+    cd m8m
+    cp arch/arm/boot/zImage ../kernel_output_flar2/zImage
+    find . -name "*.ko" -exec cp {} ../kernel_output_flar2 \;
+else
+    mkdir ../kernel_output_flar2
+    cp arch/arm/boot/zImage ../kernel_output_flar2/zImage
+    find . -name "*.ko" -exec cp {} ../kernel_output_flar2 \;
+fi
